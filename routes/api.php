9f0ed7f60e7e13de3f25eeac05714cae9f3a4f94
lumiflare <shim@shimui-MacBookPro.local>
2026-01-21 21:28:40 +0900

    delete users table based auth and APIs

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,6 @@
 
 declare(strict_types=1);
 
-use App\UserInterface\User\Controller\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('users')->group(function (): void {
-    Route::post('/', [UserController::class, 'store']);
-    Route::get('/{id}', [UserController::class, 'show']);
-});
+Route::middleware('api')->group(function (): void {});
